c4aeb1e6216fbd5a641e2381a9eaa4940e0f3361
anupbista <bistaanup77@gmail.com>
2020-05-28 09:30:11 +0545

    Slider responsiveness

--- src/components/HomePage/LatestBooks/index.js
+++ src/components/HomePage/LatestBooks/index.js
@@ -13,6 +13,29 @@ export default function LatestBooks({ book }) {
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <MainBook>
