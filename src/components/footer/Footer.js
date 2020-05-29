209e0ff79f8d44803102cef1723e120f16dd538a
Nikhil Nepal <nikhilnepal@users.noreply.github.com>
2020-05-29 20:04:19 +0545

    Footer update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,13 +3,16 @@ import { MDBContainer, MDBFooter } from "mdbreact";
 
 const FooterPage = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          Copyright &copy; {new Date().getFullYear()} | SANDR Group
-        </MDBContainer>
-      </div>
-    </MDBFooter>
+    // <MDBFooter color="blue" className="font-small pt-4 mt-4">
+    //   <div className="footer-copyright text-center py-3">
+    //     <MDBContainer fluid>
+    //       Copyright &copy; {new Date().getFullYear()} | SANDR Group
+    //     </MDBContainer>
+    //   </div>
+    // </MDBFooter>
+    <div className="footer">
+      <p>Copyright &copy; {new Date().getFullYear()} | SANDR Group</p>
+    </div>
   );
 };
 
