4433373f06d2d05ebc625808c228f0e92e31c49d
Nikhil Nepal <nikhilnepal@users.noreply.github.com>
2020-05-31 20:32:08 +0545

    videos edited

--- src/components/YouTubeVideos/index.js
+++ src/components/YouTubeVideos/index.js
@@ -4,33 +4,35 @@ import { Link } from "react-router-dom";
 import { Theme } from "./style";
 import Axios from "axios";
 import EmptyState from "../shared/empty";
-import { GlobalContext } from '../../contexts/GlobalContext';
-import ReactPaginate from 'react-paginate';
+import { GlobalContext } from "../../contexts/GlobalContext";
+import ReactPaginate from "react-paginate";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
-import ReactPlayer from "react-player"
+import ReactPlayer from "react-player";
 
-export default function Videoss(props) {
+export default function Videos(props) {
   const search = props.location.state?.search
     ? props.location.state.search
     : "";
   const category = props.match.params?.id ? props.match.params.id : "";
 
-  const [latestVideoss, setlatestVideoss] = useState([]);
+  const [latestVideos, setlatestVideos] = useState([]);
   const [page, setPage] = useState({
     perPage: 15,
-    currentPage: 0
+    currentPage: 0,
   });
   const [pageCount, setPageCount] = useState(0);
 
   const { loading, toggleLoading } = useContext(GlobalContext);
   const fetchData = async () => {
-    toggleLoading(true)
+    toggleLoading(true);
     const res = await Axios.get(
-      `https://ent-api-dev.herokuapp.com/api/v1/youtubevideos?search=${search}&category=${category}&page=${page.currentPage + 1}&limit=${page.perPage}`
+      `https://ent-api-dev.herokuapp.com/api/v1/youtubevideos?search=${search}&category=${category}&page=${
+        page.currentPage + 1
+      }&limit=${page.perPage}`
     );
-    setlatestVideoss(res.data.data);
-    setPageCount(res.data.count / 15)
-    toggleLoading(false)
+    setlatestVideos(res.data.data);
+    setPageCount(res.data.count / 15);
+    toggleLoading(false);
   };
 
   useEffect(() => {
@@ -47,47 +49,63 @@ export default function Videoss(props) {
       ...page,
       currentPage: selectedPage,
     });
-  }
+  };
 
   return (
-    <Fragment>
-      {!loading ? <Container fluid>
-        <div className="clearfix mt-5 mb-5">
-          <h4 className="float-left" className="title">Videoss</h4>
-        </div>
-        <Row>
-          {latestVideoss.length > 0 ? latestVideoss.map(function (video) {
-            return (
-              <Col md={4} key={video.id} className="list-item">
-                <Link to={`/youtubevideos/${video.id}`}>
-                  <Card>
-                    <ReactPlayer width={'100%'} height={'100%'}
-                      url={video.watchlink} />
-                    <Card.Body className="ellipsis">
-                      <span>{video.name}</span>
-                    </Card.Body>
-                  </Card>
-                </Link>
-              </Col>
-            );
-          }) : <EmptyState />}
-        </Row>
-        <Row className="justify-content-center">
-          {latestVideoss.length > 0 ? <ReactPaginate
-            previousLabel={<FaAngleLeft />}
-            nextLabel={<FaAngleRight />}
-            breakLabel={"..."}
-            breakClassName={"break-me"}
-            pageCount={pageCount}
-            marginPagesDisplayed={2}
-            forcePage={page.currentPage}
-            pageRangeDisplayed={5}
-            onPageChange={handlePageChange}
-            containerClassName={"pagination"}
-            subContainerClassName={"pages pagination"}
-            activeClassName={"active"} /> : null}
-        </Row>
-      </Container> : null}
-    </Fragment>
-  )
+    <Theme>
+      <Fragment>
+        {!loading ? (
+          <Container fluid>
+            <div className="clearfix mt-5 mb-5">
+              <h4 className="float-left" className="title">
+                Videos
+              </h4>
+            </div>
+            <Row>
+              {latestVideos.length > 0 ? (
+                latestVideos.map(function (video) {
+                  return (
+                    <Col md={4} key={video.id} className="list-item">
+                      <Link to={`/youtubevideos/${video.id}`}>
+                        <Card>
+                          <ReactPlayer
+                            width={"100%"}
+                            height={"100%"}
+                            url={video.watchlink}
+                          />
+                          <Card.Body className="ellipsis">
+                            <span>{video.name}</span>
+                          </Card.Body>
+                        </Card>
+                      </Link>
+                    </Col>
+                  );
+                })
+              ) : (
+                <EmptyState />
+              )}
+            </Row>
+            <Row className="justify-content-center">
+              {latestVideos.length > 0 ? (
+                <ReactPaginate
+                  previousLabel={<FaAngleLeft />}
+                  nextLabel={<FaAngleRight />}
+                  breakLabel={"..."}
+                  breakClassName={"break-me"}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={2}
+                  forcePage={page.currentPage}
+                  pageRangeDisplayed={5}
+                  onPageChange={handlePageChange}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}
+                />
+              ) : null}
+            </Row>
+          </Container>
+        ) : null}
+      </Fragment>
+    </Theme>
+  );
 }
